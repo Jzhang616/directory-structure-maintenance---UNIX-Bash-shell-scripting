784040370c8c14e0c6e66ab5f6917cf204612835
Jason Zhang <141682791+Jzhang616@users.noreply.github.com>
2023-08-08 16:45:31 -0400

    Add files via upload

---
+++ p1.sh
@@ -0,0 +1,23 @@
+#Jason Zhang
+#JASZHANG
+#ID: 112920652
+
+
+#!/bin/bash 
+
+if [ $# -ne 3 ]; then 
+	echo 'USEAGE: p1.sh <extension> <src_dir> <dst_Dir>'
+else
+	file_type=$1
+	src_dir=$2
+	dest_dir=$3
+	if [ -d "$src_dir" ]; then 
+		mkdir -p "$dest_dir"
+		find "$src_dir" -type f -name "*$file_type" -exec mv {} "$dest_dir" \;
+		#does not move files in sub-directories
+	else
+		echo "<$src_dir> not found"
+		exit 0
+	fi	
+fi
+
